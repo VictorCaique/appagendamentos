6b6d5371890e2d284bc551023aab63fe92d3a0e5
VictorCaique <vcds4747@gmail.com>
2021-09-10 16:29:16 -0300

    Tela Editar

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDIibmqDloJYkmU6ZD3IsLPdTwIubM1zNc",
+    authDomain: "app-agendamentos-cbb29.firebaseapp.com",
+    projectId: "app-agendamentos-cbb29",
+    storageBucket: "app-agendamentos-cbb29.appspot.com",
+    messagingSenderId: "714590150114",
+    appId: "1:714590150114:web:c75323b2f61e0084afadbb",
+    measurementId: "G-351QCS58P8"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
